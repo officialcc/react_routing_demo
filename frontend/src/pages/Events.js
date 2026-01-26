d8019cb1023f2d646c5dbb9a46aeb2402ea875e0
colin <163393924+officialcc@users.noreply.github.com>
2026-01-26 13:20:39 +0800

    Completed Lesson 371

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -34,6 +34,11 @@ function EventsPage() {
 //     </>
 //   );
   const data = useLoaderData();
+
+//   if (data.isError) {
+//     return <p>{data.message}</p>
+//   }
+
   const events = data.events;
 
   return (
@@ -47,7 +52,18 @@ export async function loader() {
     const response = await fetch('http://localhost:8080/events');
               
     if (!response.ok) {
-        // ...
+        // return {isError: true, message: 'Could not fetch events.'};
+        throw new Response(JSON.stringify({ message: 'Could not fetch events.'}), {
+            status: 500,
+        });
+        // -- For react-router-dom v6 only, not applicable for v7 --
+        // -- Must import json from react-router-dom as well --
+        // throw json(
+        //     { message: 'Could not fetch events.'},
+        //     {
+        //         status: 500,
+        //     }
+        // );
     } else {
         // const resData = await response.json();
         // return resData.events;
