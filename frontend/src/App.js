21f8725d20b782e7de90e6e091f12e9d0f4e1aa5
colin <163393924+officialcc@users.noreply.github.com>
2026-01-28 00:54:01 +0800

    Completed Lesson 379

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,12 +23,16 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 import HomePage from './pages/Home';
 import EventsPage, { loader as eventsLoader } from './pages/Events';
-import EventDetailPage, { loader as eventDetailLoader } from './pages/EventDetail';
+import EventDetailPage, {
+  loader as eventDetailLoader,
+  action as deleteEventAction,
+} from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
 import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRoot';
 import ErrorPage from './pages/Error';
+import { action as manipulateEventAction } from './components/EventForm';
 
 const  router = createBrowserRouter([
   {
@@ -54,11 +58,20 @@ const  router = createBrowserRouter([
               {
                 index: true,
                 element: <EventDetailPage />,
+                action: deleteEventAction,
               },
-              { path: 'edit', element: <EditEventPage /> },
+              {
+                path: 'edit',
+                element: <EditEventPage />,
+                action: manipulateEventAction
+              },
             ]
           },
-          { path: 'new', element: <NewEventPage /> },
+          {
+            path: 'new',
+            element: <NewEventPage />, 
+            action: manipulateEventAction
+          },
         ],
       },
     ],
--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,4 @@
-import { useRouteLoaderData } from "react-router-dom";
+import { redirect, useRouteLoaderData } from "react-router-dom";
 
 import EventItem from "../components/EventItem";
 
@@ -25,4 +25,19 @@ export async function loader({ request, params }) {
     } else {
         return response;
     }
+}
+
+export async function action({ params, request }) {
+    const eventId = params.eventId;
+    const response = await fetch('http://localhost:8080/events/' + eventId, {
+        method: request.method,
+    });
+
+    if (!response.ok) {
+        throw new Response(JSON.stringify({ message: 'Could not delete event.' }), {
+            status: 500,
+        });
+    }
+
+    return redirect('/events');
 }
